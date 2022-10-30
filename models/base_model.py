2ee3423836b09542db04f77fa46a141c783ea0f2
Mubarek Seid <mubareksd@gmail.com>
2022-10-30 11:40:47 +0300

    task 5

--- models/base_model.py
+++ models/base_model.py
@@ -4,7 +4,7 @@ base_model module
 """
 from uuid import uuid4
 from datetime import datetime
-
+from models import storage
 
 class BaseModel:
     """
@@ -24,6 +24,7 @@ class BaseModel:
             self.id = str(uuid4())
             self.created_at = datetime.now()
             self.updated_at = self.created_at
+            storage.new(self)
 
     def __str__(self):
         """returns string representation"""
@@ -34,6 +35,7 @@ class BaseModel:
         """updates the public instance attribute
         updated_at with the current datetime"""
         self.updated_at = datetime.now()
+        storage.save()
 
     def to_dict(self):
         """returns a dictionary containing all keys/vlues
