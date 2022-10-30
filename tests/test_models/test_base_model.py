740f4a766b242d4f45bee5b4594f3a738e751a4c
Mubarek Seid <mubareksd@gmail.com>
2022-10-30 17:28:32 +0300

    test for bas_model

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -2,14 +2,14 @@
 """
 test_base_model module
 """
-import unittest
+from unittest import TestCase
 from uuid import uuid4
 from datetime import datetime
-import time
+from time import sleep
 from models.base_model import BaseModel
 
 
-class TestBaseModel(unittest.TestCase):
+class TestBaseModel(TestCase):
     """
     Test cases for the BaseModel class
     """
@@ -22,18 +22,55 @@ class TestBaseModel(unittest.TestCase):
         """"""
         pass
 
-    def test3(self):
-        """"""
-        my_model = BaseModel()
+    def test_module_doc(self):
+        """test module documentation"""
+        doc = __import__("models.base_model").__doc__
+        self.assertGreater(len(doc), 1)
 
-    def test3save(self):
-        """"""
-        my_model = BaseModel()
-        time.sleep(0.5)
-        current = datetime.now()
-        my_model.save()
+    def test_class_doc(self):
+        """test class documentation"""
+        doc = BaseModel.__doc__
+        self.assertGreater(len(doc), 1)
 
-    def test3dict(self):
+    def test_init_doc(self):
+        """test init method documentation"""
+        doc = BaseModel.__init__.__doc__
+        self.assertGreater(len(doc), 1)
+
+    def test_str_doc(self):
+        """test str functiom documentation"""
+        doc = BaseModel.__str__.__doc__
+        self.assertGreater(len(doc), 1)
+
+    def test_save_doc(self):
+        """test save method documentation"""
+        doc = BaseModel.save.__doc__
+        self.assertGreater(len(doc), 1)
+
+    def test_to_dict_doc(self):
+        """test to_dict method documentation"""
+        doc = BaseModel.to_dict.__doc__
+        self.assertGreater(len(doc), 1)
+
+    def test_init(self):
         """"""
-        my_model = BaseModel()
-        my_model_dict = my_model.to_dict()
+        obj = BaseModel()
+
+        self.assertIsInstance(obj, BaseModel)
+        self.assertIsInstance(obj.id, str)
+        self.assertIsInstance(obj.updated_at, datetime)
+        self.assertIsInstance(obj.created_at, datetime)
+
+    def test_save(self):
+        """test save method"""
+        obj = BaseModel()
+        sleep(0.5)
+
+        now = datetime.now().replace(microsecond=0)
+        obj.save()
+        self.assertEqual(obj.updated_at.replace(microsecond=0), now)
+
+    def test_to_dict(self):
+        """test to_dict method"""
+        obj = BaseModel()
+        my_model_dict = obj.to_dict()
