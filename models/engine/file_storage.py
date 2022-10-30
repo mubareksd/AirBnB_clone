948870acca7f320d9c98ee1849f574f17fafdf9d
Mubarek Seid <mubareksd@gmail.com>
2022-10-30 11:37:02 +0300

    task 5

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -18,16 +18,18 @@ class FileStorage:
 
     def new(self, obj):
         """sets in __objects the obj with key <obj class name>.id"""
-        self.__objects["{}.{}.format(type(obj).__name__, obj.id)"] = obj
+        self.__objects["{}.{}.\
+            format(type(obj).__name__, obj.id)"] = obj
 
     def save(self):
         """serializes __objects to the JSON file (path: __file_path)"""
         with open(self.__file_path, mode="w") as f:
-            dict_srorage = {key: value.to_dict() for key, value in self.__objects.items()}
+            dict_srorage = {k: v.to_dict() for k, v in self.__objects.items()}
             json.dump(dict_storage, f)
 
     def reload(self):
         """
-        deserializes the JSON file to __objects (only if the JSON file (__file_path) exists ; otherwise,
+        deserializes the JSON file to __objects
+        (only if the JSON file (__file_path) exists ; otherwise,
         do nothing. If the file doesn’t exist, no exception should be raised)
         """
