002a1ca5543584d90d30756ec64d0d4c086ed7e0
Mubarek Seid <mubareksd@gmail.com>
2022-10-30 19:59:41 +0300

    task 3

--- models/base_model.py
+++ models/base_model.py
@@ -14,7 +14,7 @@ class BaseModel:
 
     def __init__(self, *args, **kwargs):
         """initialization"""
-        if kwargs != {}:
+        if kwargs is not None and kwargs != {}:
             for key, value in kwargs.items():
                 if key != "__class__":
                     if key in ('created_at', 'updated_at'):
