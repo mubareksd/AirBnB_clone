32c853c33bb58445fef36ccf6315e0baa2ecad77
Mubarek Seid <mubareksd@gmail.com>
2022-10-30 21:55:14 +0300

    update

--- console.py
+++ console.py
@@ -3,6 +3,8 @@
 console module
 """
 import cmd
+from models.base_model import BaseModel
+from models.engine.file_storage import FileStorage
 
 
 class HBNBCommand(cmd.Cmd):
@@ -24,6 +26,86 @@ class HBNBCommand(cmd.Cmd):
     def emptyline(self):
         """an empty line"""
         pass
+
+    def do_create(self, line):
+        """Creates a new instance of BaseModel"""
+        if line == "" or line is None:
+            print("** class name missing **")
+        elif line != "BaseModel":
+            print("** class doesn't exist **")
+        else:
+            obj = BaseModel()
+            print(obj.id)
+
+    def do_show(self, line):
+        """Prints the string representation of an
+        instance based on the class name and id"""
+        if line == "" or line is None:
+            print("** class name missing **")
+        else:
+            args = line.split(' ')
+            if args[0] != "BaseModel":
+                print("** class doesn't exist **")
+            elif len(args) < 2:
+                print("** instance id missing **")
+            else:
+                key = "{}.{}".format(args[0], args[1])
+                if key not in FileStorage._FileStorage__objects:
+                    print("** no instance found **")
+                else:
+                    print(FileStorage._FileStorage__objects[key])
+
+    def do_destroy(self, line):
+        """Deletes an instance based on the class name and id"""
+        if line == "" or line is None:
+            print("** class name missing **")
+        else:
+            args = line.split(' ')
+            if args[0] != "BaseModel":
+                print("** class doesn't exist **")
+            elif len(args) < 2:
+                print("** instance id missing **")
+            else:
+                key = "{}.{}".format(args[0], args[1])
+                if key not in FileStorage._FileStorage__objects:
+                    print("** no instance found **")
+                else:
+                    FileStorage._FileStorage__objects[key].save()
+                    del FileStorage._FileStorage__objects[key]
+
+    def do_all(self, line):
+        """Prints all string representation of all
+        instances based or not on the class name"""
+        if line != "":
+            words = line.split(' ')
+            if words[0] != "BaseModel":
+                print("** class doesn't exist **")
+        for k in FileStorage._FileStorage__objects:
+            print(FileStorage._FileStorage__objects[k])
+
+    def do_update(self, line):
+        """Updates an instance based on the class name
+        and id by adding or updating attribute """
+        if line == "" or line is None:
+            print("** class name missing **")
+        else:
+            words = line.split(' ')
+            if words[0] != "BaseModel":
+                print("** class doesn't exist **")
+            elif len(words) < 2:
+                print("** instance id missing **")
+            elif len(words) < 3:
+                print("** attribute name missing **")
+            elif len(words) < 4:
+                print("** value missing **")
+            else:
+                key = "{}.{}".format(words[0], words[1])
+                if key not in FileStorage._FileStorage__objects:
+                    print("** no instance found **")
+                else:
+                    setattr(
+                        FileStorage._FileStorage__objects[key],
+                        words[2], words[3])
 
 
 if __name__ == '__main__':
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -43,5 +43,4 @@ class FileStorage:
             obj_dict = json.load(f)
             obj_dict = {k: classes[v["__class__"]]
                         (**v) for k, v in obj_dict.items()}
-            # TODO: should this overwrite or insert?
             FileStorage.__objects = obj_dict
